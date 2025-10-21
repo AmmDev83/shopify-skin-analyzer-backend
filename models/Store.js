91e795f47d1005b637e864ec3a186c8c328c3f8f
AmmDev83 <a.molinamoyano@gmail.com>
2025-10-21 20:51:45 +0200

    Se corrije el error de levantar el servidor

--- models/Store.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const storeSchema = new mongoose.Schema({
-    shop: { type: String, required: true, unique: true },
-    accessToken: { type: String, required: true },
-    settings: { type: Object, default: {} }
-});
-
-module.exports = mongoose.model("Store", storeSchema);
